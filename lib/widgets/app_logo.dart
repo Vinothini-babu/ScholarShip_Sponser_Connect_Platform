baaa16c0ab77ac64c0e6d4e17c76f04eaeb90fa4
PoeageHub <Poeagetechnology@gmail.com>
2026-07-29 12:10:38 +0530

    22th commit

--- lib/widgets/app_logo.dart
+++ lib/widgets/app_logo.dart
@@ -11,7 +11,7 @@ class AppLogo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Image.asset(
-      "assets/images/logo1.png",
+      "assets/images/app_logo.png",
       height: size,
     );
   }
